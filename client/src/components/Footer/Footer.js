c6f54a9629347585f22e793d7d6a3647bba2c13b
omar <omar96_96@hotmail.com>
2021-08-09 02:39:58 +0200

    home page responsive

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -12,18 +12,27 @@ function Footer(props) {
     return (
         <div className='footer'>
 
-          <img src="./assets/logo-black-icon.svg" height="70%" alt="" />
-          <div className='footer-left'>
+         <div className="footer-left">
+              <img  src="./assets/logo-black-icon.svg" height="70%" alt="" />
+        </div>
+          <div className='footer-right'>
+            <div className='footer-icons display-block'>
+                <i className="fas fa-envelope"></i>
+                <i className="fab fa-twitter"></i>
+                <i className="fab fa-instagram"></i>
+                <i className="fab fa-behance"></i>
+                <i className="fab fa-dribbble"></i>
+            </div>
             <div>
                 +44 (0) 20 8347 7777 
             </div>
-            <div> 
+            <div className='display-none'> 
                 |
             </div>
             <div>
                 hello@fishfinger.me
             </div>
-            <div className='footer-icons'>
+            <div className='footer-icons display-none'>
                 <i className="fas fa-envelope"></i>
                 <i className="fab fa-twitter"></i>
                 <i className="fab fa-instagram"></i>
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Col, Container, Jumbotron, Row } from "reactstrap";
+import { div, Container, Jumbotron, Row } from "reactstrap";
 import Footer from "../../components/Footer/Footer";
 import "./home.scss";
 function Home(props) {
@@ -23,45 +23,108 @@ function Home(props) {
     }
   };
 
+  
+
+//   var w = window.matchMedia("(max-width: 700px)");
+//   var vid = document.getElementById("vid");
+//   var source = document.createElement("source");
+//   source.id = "hvid";
+//   source.setAttribute("type", "video/mp4");
+//   vid.appendChild(source);
+  
+//   if (w.matches) {
+//     // vid.pause();
+//     source.removeAttribute("src");
+//     source.setAttribute("src", "./assets/Magicians_Mobile_5_gfyhw.mp4");
+//     // vid.load();
+//     // vid.play();
+//   } else {
+//     // vid.pause();
+//     source.removeAttribute("src");
+//     source.setAttribute("src", "./assets/Magicians_3_vzbge.mp4");
+//     // vid.load();
+//     // vid.play();
+//   }
+
+// window.addEventListener("resize", function(){
+//   var w = window.matchMedia("(max-width: 700px)");
+//   // var vid = document.getElementById("vid");
+//   var source = document.getElementById("hvid");
+  
+//   if (w.matches) {
+//     // vid.pause();
+//     source.src = "./assets/Magicians_Mobile_5_gfyhw.mp4";
+//     // vid.load();
+//     // vid.play();
+//   } else {
+//     // vid.pause();
+//     source.src = "./assets/Magicians_3_vzbge.mp4";
+//     // vid.load();
+//     // vid.play();
+//   }
+// });
   const scrollDown=()=>{
-    window.scrollTo({
-      top: 620,
-      behavior: 'smooth'
-    });
+    window.scrollBy(0, window.innerHeight);
+        // window.scrollTo({
+    //   top: 620,
+    //   behavior: 'smooth'
+    // });
   }
   useEffect(() => {
+    var w = window.matchMedia("(max-width: 700px)");
+  var vid = document.getElementById("vid");
+  var source = document.createElement("source");
+  source.id = "hvid";
+  source.setAttribute("type", "video/mp4");
+  vid.appendChild(source);
+  
+  if (w.matches) {
+    vid.pause();
+    source.removeAttribute("src");
+    source.setAttribute("src", "./assets/Magicians_Mobile_5_gfyhw.mp4");
+    vid.load();
+    vid.play();
+  } else {
+    vid.pause();
+    source.removeAttribute("src");
+    source.setAttribute("src", "./assets/Magicians_3_vzbge.mp4");
+    vid.load();
+    vid.play();
+  }
+
+window.addEventListener("resize", function(){
+  var w = window.matchMedia("(max-width: 700px)");
+  var vid = document.getElementById("vid");
+  var source = document.getElementById("hvid");
+  
+  if (w.matches) {
+    vid.pause();
+    source.src = "./assets/Magicians_Mobile_5_gfyhw.mp4";
+    vid.load();
+    vid.play();
+  } else {
+    vid.pause();
+    source.src = "./assets/Magicians_3_vzbge.mp4";
+    vid.load();
+    vid.play();
+  }
+});
     return () => {};
   }, []);
 
   return (
     <>
-      {/* <video class="bg-media__video js-header-video" 
-            autoplay="" loop="" muted="" playsinline="true" 
-            data-desktop=
-            "https://fishfinger.imgix.net/storage/uploads/homepage/Homepage3/Magicians_3_vzbge.mp4" 
-            data-320=
-            "https://fishfinger.imgix.net/storage/uploads/homepage/Homepage3/Magicians%20Mobile_5_gfyhw.mp4" 
-            data-375=
-            "https://fishfinger.imgix.net/storage/uploads/homepage/Homepage3/Magicians%20Mobile_5_gfyhw.mp4" 
-            data-414=
-            "https://fishfinger.imgix.net/storage/uploads/homepage/Homepage3/Magicians%20Mobile_5_gfyhw.mp4" 
-            data-768=
-            "https://fishfinger.imgix.net/storage/uploads/homepage/Homepage3/magiciansipad_yceic.mp4"> 
-                 <source src="https://fishfinger.imgix.net/storage/uploads/homepage/Homepage3/Magicians_3_vzbge.mp4" 
-                data-src=
-                "https://fishfinger.imgix.net/storage/uploads/homepage/Homepage3/Magicians_3_vzbge.mp4" 
-                type="video/mp4"></source> 
-                </video> */}
-      <container id="videoContainer">
-        <video
+      <div id="videoContainer">
+        <video id="vid"
           width="100%"
           loop="true"
           autoplay="autoplay"
           muted
-          src="./assets/Magicians_3_vzbge.mp4"
-          type="video/mp4"
         >
-          {/* <source src="./assets/Magicians_3_vzbge.mp4" type="video/mp4"/> */}
+          {/* <source src="./assets/Magicians_Mobile_5_gfyhw.mp4" type="video/mp4"  media="all and (max-width: 600px)"/>
+          <source src="./assets/Magicians_3_vzbge.mp4" type="video/mp4" media="all and (max-width: 1500px)"/> */}
+          {/* <source id="hvid" src="./assets/Magicians_3_vzbge.mp4" type="video/mp4" /> */}
+          {/* <video src="./assets/Magicians_3_vzbge.mp4" style={{width:"auto"}}/> */}
           Your browser does not support the video tag.
         </video>
         <div id="vidTxt">
@@ -84,58 +147,62 @@ function Home(props) {
           </div>
           <br />
           <br />
-          <Container>
-            <Row>
-              <Col xs="1"></Col>
-              <Col xs="1">
+          <div className="container">
+            <div class="row">
+              {/* <div className="col-1 col-lg-1"></div> */}
+              <div className="col-lg-1 space" ></div>
+
+              <div className="col-4 col-lg-2">
                 <img src="./assets/nike.svg" alt="" />
-              </Col>
-              <Col xs="1"></Col>
-              <Col xs="1">
+              </div>
+              {/* <div className="col-4 col-lg-2"></div> */}
+              <div className="col-4 col-lg-2">
                 <img src="./assets/google.svg" alt="" />
-              </Col>
-              <Col xs="1"></Col>
-              <Col xs="1">
+              </div>
+              {/* <div className="col-4 col-lg-2"></div> */}
+              <div className="col-4 col-lg-2">
                 <img src="./assets/wwf.svg" alt="" />
-              </Col>
-              <Col xs="1"></Col>
-              <Col xs="1">
+              </div>
+              {/* <div className="col-4 col-lg-2"></div> */}
+              <div className="col-4 col-lg-2">
                 <img src="./assets/red-bull.svg" alt="" />
-              </Col>
-              <Col xs="1"></Col>
-              <Col xs="1">
+              </div>
+              {/* <div className="col-4 col-lg-2"></div> */}
+              <div className="col-4 col-lg-2">
                 <img src="./assets/king.svg" alt="" />
-              </Col>
-              <Col xs="3"></Col>
-            </Row>
+              </div>
+              <div className="col-lg-1 space" ></div>
+              <div className="col-lg-2 space" ></div>
+
+            {/* </Row>
             <br />
             <br />
-            <Row>
-              <Col xs="2"></Col>
-              <Col xs="1">
+            <Row> */}
+              {/* <div md="1"></div> */}
+              <div className="col-4 col-lg-2">
                 <img src="./assets/adidas.svg" alt="" />
-              </Col>
-              <Col xs="1"></Col>
-              <Col xs="1">
+              </div>
+              {/* <div className="col-4 col-lg-2"></div> */}
+              <div className="col-4 col-lg-2">
                 <img src="./assets/riot-games.svg" alt="" />
-              </Col>
-              <Col xs="1"></Col>
-              <Col xs="1">
+              </div>
+              {/* <div className="col-4 col-lg-2"></div> */}
+              <div className="col-4 col-lg-2">
                 <img src="./assets/penguin.svg" alt="" />
-              </Col>
-              <Col xs="1"></Col>
-              <Col xs="1">
+              </div>
+              {/* <div className="col-4 col-lg-2"></div> */}
+              <div className="col-4 col-lg-2">
                 <img src="./assets/nhs.svg" alt="" />
-              </Col>
-              <Col xs="1"></Col>
-            </Row>
-          </Container>
+              </div>
+              {/* <div className="col-4 col-lg-2"></div> */}
+            </div>
+          </div>
           <br />
           <br />
         
             <button className='button'>FIND OUT MORE</button>
         </Jumbotron>
-      </container>
+      </div>
       <Footer />
     
     </>
